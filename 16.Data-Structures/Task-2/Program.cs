dea013498e0fda95170c4fc2094d0dc12d30a786
KRONIK666 <kronik@abv.bg>
2017-02-02 17:39:48 +0200

    Tasks and Examples

---
+++ 16.Data-Structures/Task-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Add a number: ");
+                int n = int.Parse(Console.ReadLine());
+                stack.Push(n);
+            } Console.WriteLine();
+
+            Console.WriteLine("Numbers in opposite order:");
+            Console.WriteLine();
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            } Console.WriteLine();
+        }
+    }
+}
---
+++ 16.Data-Structures/Task-4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write("Add a number: ");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num > 0)
+                {
+                    numbers.Add(num);
+                }
+                else
+                {
+                    continue;
+                }
+            } Console.WriteLine();
+
+            Console.WriteLine("Positive numbers added to the list: ");
+            Console.WriteLine();
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            } Console.WriteLine();
+        }
+    }
+}
